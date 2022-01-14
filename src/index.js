66e5e7238151d6667cc50789e9d12df2969dd993
nickrally <nickrally2009@gmail.com>
2022-01-13 21:53:26 -0700

    copy plugin works with context

--- src/index.js
+++ src/index.js
@@ -15,6 +15,18 @@ function component() {
   btn2.onclick = ohNoes;
   element.appendChild(btn2);
 
+  const img1 = document.createElement("img");
+  img1.src = "images/mn.png";
+  element.append(img1);
+
+  const img2 = document.createElement("img");
+  img2.src = "other-images/va.gif";
+  element.append(img2);
+
+  const img3 = document.createElement("img");
+  img3.src = "other-images/av.gif";
+  element.append(img3);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -7,7 +8,18 @@ module.exports = {
     print: "./src/print.js",
     ohnoes: "./src/ohnoes.js",
   },
-  plugins: [new HtmlWebpackPlugin({ title: "Output Management" })],
+  plugins: [
+    new HtmlWebpackPlugin({ title: "Output Management" }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "**/*",
+          to: ".",
+          context: path.resolve(__dirname, "assets/visual"),
+        },
+      ],
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
